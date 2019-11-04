e0ae9ccf635a5d17af0e39b9f0a76cb5559f3d2d
Salie Lim <salie.lim@gmail.com>
2019-11-05 09:33:58 +1100

    setup HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <div>Hello Worlddd!!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'main.js',
+    filename: 'index.js',
   },
   module: {
     rules: [
       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
