581e695966fb0609da28b38d45f51b07454eb86b
Salie Lim <salie.lim@gmail.com>
2019-11-05 11:21:03 +1100

    add api call to beer-temp-monitor-backend

--- src/index.js
+++ src/index.js
@@ -1,18 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    temp1: '',
+    temp2: '',
+    temp3: '',
+    temp4: '',
+    temp5: '',
+    temp6: '',
+  };
+
+  componentDidMount() {
+    axios
+      .post(
+        `https://us-central1-noderite-crawler.cloudfunctions.net/beer-temp-monitor-backend`,
+        {
+          id: 1,
+        },
+      )
+      .then(res => {
+        const temp1 = res.data.data.temperature;
+        this.setState({ temp1 });
+      });
+
+    axios
+      .post(
+        `https://us-central1-noderite-crawler.cloudfunctions.net/beer-temp-monitor-backend`,
+        {
+          id: 2,
+        },
+      )
+      .then(res => {
+        const temp2 = res.data.data.temperature;
+        this.setState({ temp2 });
+      });
+
+    axios
+      .post(
+        `https://us-central1-noderite-crawler.cloudfunctions.net/beer-temp-monitor-backend`,
+        {
+          id: 3,
+        },
+      )
+      .then(res => {
+        const temp3 = res.data.data.temperature;
+        this.setState({ temp3 });
+      });
+
+    axios
+      .post(
+        `https://us-central1-noderite-crawler.cloudfunctions.net/beer-temp-monitor-backend`,
+        {
+          id: 4,
+        },
+      )
+      .then(res => {
+        const temp4 = res.data.data.temperature;
+        this.setState({ temp4 });
+      });
+
+    axios
+      .post(
+        `https://us-central1-noderite-crawler.cloudfunctions.net/beer-temp-monitor-backend`,
+        {
+          id: 5,
+        },
+      )
+      .then(res => {
+        const temp5 = res.data.data.temperature;
+        this.setState({ temp5 });
+      });
+
+    axios
+      .post(
+        `https://us-central1-noderite-crawler.cloudfunctions.net/beer-temp-monitor-backend`,
+        {
+          id: 6,
+        },
+      )
+      .then(res => {
+        const temp6 = res.data.data.temperature;
+        this.setState({ temp6 });
+      });
+  }
+
   render() {
+    const { temp1, temp2, temp3, temp4, temp5, temp6 } = this.state;
     return (
       <div>
         <h1>Beer Temperature Monitoring</h1>
-        <table class="table table-dark">
+        <table className="table table-dark">
           <thead>
             <tr>
               <th scope="col">#</th>
               <th scope="col">Beer Type</th>
               <th scope="col">Min Temp</th>
               <th scope="col">Max Temp</th>
+              <th scope="col">Current Temp</th>
             </tr>
           </thead>
           <tbody>
@@ -21,36 +107,42 @@ class App extends React.Component {
               <td>Pilsner</td>
               <td>4</td>
               <td>6</td>
+              <td>{temp1}</td>
             </tr>
             <tr>
               <th scope="row">2</th>
               <td>IPA</td>
               <td>5</td>
               <td>6</td>
+              <td>{temp2}</td>
             </tr>
             <tr>
               <th scope="row">3</th>
               <td>Lager</td>
               <td>4</td>
               <td>7</td>
+              <td>{temp3}</td>
             </tr>
             <tr>
               <th scope="row">4</th>
               <td>Stout</td>
               <td>6</td>
               <td>8</td>
+              <td>{temp4}</td>
             </tr>
             <tr>
               <th scope="row">5</th>
               <td>Wheat beer</td>
               <td>3</td>
               <td>5</td>
+              <td>{temp5}</td>
             </tr>
             <tr>
-              <th scope="row">5</th>
+              <th scope="row">6</th>
               <td>Pale Ale</td>
               <td>4</td>
               <td>6</td>
+              <td>{temp6}</td>
             </tr>
           </tbody>
         </table>
