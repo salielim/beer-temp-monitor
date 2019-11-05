e4526d55adc76cf56bc6b3ec9c0044688a0f2a4d
Salie Lim <salie.lim@gmail.com>
2019-11-05 14:36:01 +1100

    refactor beers into beers.js

---
+++ src/beers.js
@@ -0,0 +1,38 @@
+export const beers = [
+  {
+    id: '1',
+    type: 'Pilsner',
+    min: '4',
+    max: '6',
+  },
+  {
+    id: '2',
+    type: 'IPA',
+    min: '5',
+    max: '6',
+  },
+  {
+    id: '3',
+    type: 'Lager',
+    min: '4',
+    max: '7',
+  },
+  {
+    id: '4',
+    type: 'Stout',
+    min: '6',
+    max: '8',
+  },
+  {
+    id: '5',
+    type: 'Wheat beer',
+    min: '3',
+    max: '5',
+  },
+  {
+    id: '6',
+    type: 'Pale Ale',
+    min: '4',
+    max: '6',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,45 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-
-let beers = [
-  {
-    id: '1',
-    type: 'Pilsner',
-    min: '4',
-    max: '6',
-  },
-  {
-    id: '2',
-    type: 'IPA',
-    min: '5',
-    max: '6',
-  },
-  {
-    id: '3',
-    type: 'Lager',
-    min: '4',
-    max: '7',
-  },
-  {
-    id: '4',
-    type: 'Stout',
-    min: '6',
-    max: '8',
-  },
-  {
-    id: '5',
-    type: 'Wheat beer',
-    min: '3',
-    max: '5',
-  },
-  {
-    id: '6',
-    type: 'Pale Ale',
-    min: '4',
-    max: '6',
-  },
-];
+import { beers } from './beers';
 
 class App extends React.Component {
   state = {
